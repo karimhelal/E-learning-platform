cb180d1cb141ce720ef81eae53ab7cbd785bce74
Karim Helal <karimhelalkh110@gmail.com>
2025-12-04 20:17:29 +0200

    Enrollment status update

--- Masar/Core/Entities/Enums/EnrollmentStatus.cs
+++ Masar/Core/Entities/Enums/EnrollmentStatus.cs
@@ -9,5 +9,6 @@ public enum EnrollmentStatus
     Active = 1,      // Currently learning
     Completed = 2,   // Finished the course
     Inactive = 3,    // Not actively learning
-    Dropped = 4      // Withdrawn from course
+    Dropped = 4,     // Withdrawn from course
+    InProgress = 5   // Currently in progress
 }
